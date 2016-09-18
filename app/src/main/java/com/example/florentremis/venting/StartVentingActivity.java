8bab812f97a330fdf4cb60863b450501d1d0fdcd
Florent Remis <florent.remis@gmail.com>
2016-09-18 17:45:37 +0200

    Anonymous authentification and database access working

--- app/src/main/java/com/example/florentremis/venting/StartVentingActivity.java
+++ app/src/main/java/com/example/florentremis/venting/StartVentingActivity.java
@@ -1,21 +1,97 @@
 package com.example.florentremis.venting;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartVentingActivity extends AppCompatActivity {
+
+    private static final String TAG = "ViewVentRoomsActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    int i = 0;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_venting);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
 
     public void startVentRoom(View view) {
-        Intent startVentRoomIntent = new Intent(StartVentingActivity.this, VentRoomActivity.class);
-        startActivity(startVentRoomIntent);
+        if (user != null) {
+            // Write a message to the database
+            Log.d(TAG, "button clicked");
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
+            myRef.setValue("Hello, World!" + String.valueOf(i++));
+            String ventRoomId = "Id";
+            Log.d(TAG, "here");
+//        Intent startVentRoomIntent = new Intent(StartVentingActivity.this, VentRoomActivity.class);
+//        startVentRoomIntent.putExtra("ventRoomId", ventRoomId);
+//        startActivity(startVentRoomIntent);
+        } else {
+            Log.d(TAG, "Anonymous login failed");
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInAnonymously", task.getException());
+                    Toast.makeText(StartVentingActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
